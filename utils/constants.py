89ff416f02c60c8622770216ba6ca1edacea8092
ersaraujo <ersa@cin.ufpe.br>
2023-12-21 04:50:05 -0300

    create constants file

---
+++ utils/constants.py
@@ -0,0 +1,18 @@
+# Author: Elisson Rodrigo
+# Date: 21 de Dezembro de 2023
+
+SERVER              = "199.0.1.1"
+PC_TO_ROBOT_PORT    = 9600
+ROBOT_TO_PC_PORT    = 9601
+
+PWM_LOG_PREFIX      = "/motorsPWM_"
+PWM_LOG_EXTENSION   = ".csv"
+
+SLEEP_TIME          = 0.25
+MESSAGE_TIME        = 3
+TIME_BETWEEN_MSGS   = 1
+
+START_RANGE         = -25
+END_RANGE           = 25
+NUM_VALUES          = 49
+SEED_VALUE          = 42
